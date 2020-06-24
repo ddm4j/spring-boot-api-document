670afb3caf4e28c69a3ec9cf16e32c2f8c361c78
ddm4j <916951890@qq.com>
2020-06-24 21:55:30 +0800

    微调

--- src/main/java/com/github/ddm4j/api/document/annotation/ApiParams.java
+++ src/main/java/com/github/ddm4j/api/document/annotation/ApiParams.java
@@ -6,7 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.web.bind.annotation.Mapping;
 
 /**
  * 描述接口请求参数，及校验，建议所有参数使用 bean 包装，建议使用 JSON 交互(请求参数前加 @RequestBody),不支持 Map
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.Mapping;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-@Mapping
 public @interface ApiParams {
 	/**
 	 * 请求参数
